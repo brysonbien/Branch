8eefe9c78f25b6c6ec6de93c8d84e40040066eb5
Aditya Singh <adityasht@gmail.com>
2024-09-28 16:44:20 -0400

    update_profile

--- backend/database/db_writer.py
+++ backend/database/db_writer.py
@@ -5,6 +5,7 @@ from classes import *
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'genAI'))
 import interests_recommender
 import json
+from db_reader import *
 
 # Read database password from pass.txt file
 def get_db_password():
@@ -54,20 +55,26 @@ def add_user(username, password_hash, mutuals):
             connection.close()
 
 def update_user(UserOBJ):
-    extended_interest_list = get_interests_recommender(interest_list)
+    interest_list = UserOBJ.InterestList
+    extended_interest_list = interests_recommender.get_interests_recommender(interest_list)
+    password_hash = UserOBJ.Password
+    username = UserOBJ.Username
+    image = UserOBJ.Image
+    location = UserOBJ.Location
+    
     try:
         connection = get_connection()
         with connection.cursor() as cursor:
             cursor.execute("USE `UserManagement`;")
             sql = """
-            INSERT INTO `Users` (`Username`, `Image`, `InterestList`, `PasswordHash`, `Location`, `mutuals`, `Extended Interests`)
+            INSERT INTO `Users` (`Username`, `Image`, `InterestList`, `PasswordHash`, `Location`, `Extended Interests`)
             VALUES (%s, %s, %s, %s, %s, %s, %s)
             ON DUPLICATE KEY UPDATE
             `PasswordHash` = VALUES(`PasswordHash`),
             `Location` = VALUES(`Location`),
             `mutuals` = VALUES(`mutuals`);
             """
-            cursor.execute(sql, (username, image, interest_list, password_hash, location, mutuals))
+            cursor.execute(sql, (username, image, interest_list, password_hash, location, extended_interest_list))
         connection.commit()
         print(f"User '{username}' added/updated in Users table.")
     except pymysql.MySQLError as e:
@@ -79,12 +86,14 @@ def update_user(UserOBJ):
 
 if __name__ == "__main__":
     # Add a user (example)
-    tempuser = User(4)
-    tempuser.Username = 'mike wazaoski'
+    add_user('mikewazaoski', 'fakepass123', None)
+    tempuser = User(find_userid('rebelxhawk'))
+    tempuser = User(find_userid('mikewazaoski'))
+    tempuser.Username = 'mikewazaoski'
     tempuser.InterestList = ['reading', 'traveling', 'coding', 'fucking']
     tempuser.Location = 'Monsters Inc'
     tempuser.Password = 'fakepass123'
     tempuser.mutuals = ['john doe', 'jane doe']
-    add_user(tempuser)
-    print(tempuser.UserID, tempuser.Username, tempuser.InterestList, tempuser.Password, tempuser.Image, tempuser.ExtendedInterestList, tempuser.Location)
-    print(tempuser.myEventIDArr)
+    update_user(tempuser)
+    #print(tempuser.UserID, tempuser.Username, tempuser.InterestList, tempuser.Password, tempuser.Image, tempuser.ExtendedInterestList, tempuser.Location)
+    #print(tempuser.myEventIDArr)
