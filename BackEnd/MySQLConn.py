eedcd0893a9f2f838eb09a959a69ef6b28221478
Aditya Singh <adityasht@gmail.com>
2024-09-27 23:52:26 -0400

    Update MySQLConn.py

--- BackEnd/MySQLConn.py
+++ BackEnd/MySQLConn.py
@@ -5,6 +5,6 @@ connection = pymysql.connect(host='128.61.118.107',
                              port=3306,
                              user='root',
                              password='database123',
-                             db='local instance 3306',
+                             db='DBManagement',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
